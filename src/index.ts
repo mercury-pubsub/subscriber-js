df6379e054429a6dc957b58aaf4e73724eba03fd
Tanguy Michardière <54041658+tanguyMichardiere@users.noreply.github.com>
2024-08-14 12:33:31 +0200

    use channel names

--- src/index.ts
+++ src/index.ts
@@ -17,7 +17,7 @@ export class SubscriberError extends Error {
  * @public
  */
 export type SubscriberOptions = {
-	getAccessToken?(action: "pub" | "sub", channelId: keyof Channels): Promise<string>;
+	getAccessToken?(action: "pub" | "sub", channelName: keyof Channels): Promise<string>;
 };
 
 /**
@@ -26,50 +26,58 @@ export type SubscriberOptions = {
 export class Subscriber {
 	static #baseUrl = process.env.BASE_URL;
 
+	#projectId: string;
 	#getAccessToken?: SubscriberOptions["getAccessToken"];
 
-	constructor({ getAccessToken }: SubscriberOptions = {}) {
-		this.#getAccessToken = getAccessToken;
+	constructor(projectId: string, options: SubscriberOptions = {}) {
+		this.#projectId = projectId;
+		this.#getAccessToken = options.getAccessToken;
 	}
 
 	/**
 	 * @throws {SubscriberError}
 	 */
-	async publish<ChannelId extends keyof Channels>(
-		channelId: ChannelId,
-		body: Channels[ChannelId],
+	async publish<ChannelName extends keyof Channels>(
+		channelName: ChannelName,
+		body: Channels[ChannelName],
 	): Promise<void> {
 		if (this.#getAccessToken === undefined) {
 			throw new Error("Cannot publish without an access token");
 		}
-		const accessToken = await this.#getAccessToken("sub", channelId);
-		const response = await fetch(new URL(channelId, Subscriber.#baseUrl), {
-			method: "POST",
-			body: JSON.stringify(body),
-			headers: {
-				"Content-Type": "application/json",
-				// biome-ignore lint/style/useNamingConvention: standard header name
-				Authorization: `Bearer ${accessToken}`,
+		const accessToken = await this.#getAccessToken("sub", channelName);
+		const response = await fetch(
+			new URL(`${this.#projectId}/${encodeURIComponent(channelName)}`, Subscriber.#baseUrl),
+			{
+				method: "POST",
+				body: JSON.stringify(body),
+				headers: {
+					"Content-Type": "application/json",
+					// biome-ignore lint/style/useNamingConvention: standard header name
+					Authorization: `Bearer ${accessToken}`,
+				},
 			},
-		});
+		);
 		if (!response.ok) {
 			throw new SubscriberError(response.status, await response.text());
 		}
 	}
 
-	async subscribe<ChannelId extends keyof Channels>(
-		channelId: ChannelId,
-		callback: (body: Channels[ChannelId]) => void,
+	async subscribe<ChannelName extends keyof Channels>(
+		channelName: ChannelName,
+		callback: (body: Channels[ChannelName]) => void,
 		errorCallback?: (error: SubscriberError) => void,
 	): Promise<() => void> {
-		const accessToken = await this.#getAccessToken?.("sub", channelId);
-		const eventSource = new EventSource(new URL(channelId, Subscriber.#baseUrl).href, {
-			headers: {
-				"Content-Type": "application/json",
-				// biome-ignore lint/style/useNamingConvention: standard header name
-				Authorization: accessToken !== undefined ? `Bearer ${accessToken}` : undefined,
+		const accessToken = await this.#getAccessToken?.("sub", channelName);
+		const eventSource = new EventSource(
+			new URL(`${this.#projectId}/${encodeURIComponent(channelName)}`, Subscriber.#baseUrl).href,
+			{
+				headers: {
+					"Content-Type": "application/json",
+					// biome-ignore lint/style/useNamingConvention: standard header name
+					Authorization: accessToken !== undefined ? `Bearer ${accessToken}` : undefined,
+				},
 			},
-		});
+		);
 		eventSource.onmessage = (messageEvent) => {
 			callback(JSON.parse(messageEvent.data));
 		};
